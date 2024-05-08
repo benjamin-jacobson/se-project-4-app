0dcba0d68a0fc1df6282b182e5dfbe701248d2ca
BenjaminJacobson <ben.jacobson@flatironschool.com>
2024-05-08 17:24:02 -0400

    initial models.py users table

--- server/models.py
+++ server/models.py
@@ -1,4 +1,14 @@
+from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy_serializer import SerializerMixin
+
 from config import db, bcrypt
 
-# TODO database models
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String, unique = True, nullable = False)
+    _password_hash = db.Column(db.String)
+
+    __repr__(self)__:
+        return f'<id: {id}, username: {username}>'
