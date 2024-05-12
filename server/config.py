5186f8e7372af5f67b8414eccc1718ffdce56a22
BenjaminJacobson <ben.jacobson@flatironschool.com>
2024-05-12 17:11:00 -0400

    updates home page

--- server/config.py
+++ server/config.py
@@ -1,15 +1,17 @@
+import os
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
-
-# TODO setup imports
-
-# app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K' #TODO where come from
+app.secret_key = os.getenv('FLASK_APP_SECRET_KEY')
+# app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K' #TODO delete 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False # TODO what do?
